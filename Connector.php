173bc15cb38f2fa278d8fbbf7d0694d51d70d11b
Jan Dvořák <dvorak@ebrana.cz>
2014-06-23 12:17:25 +0200

    Cleaning

--- Connector.php
+++ Connector.php
@@ -1,24 +1,86 @@
 <?php
-namespace PHPsock;
+namespace Ebrana\PHPsock;
 
-require 'vendor/autoload.php';
-
+/**
+ * Connector pro napojeni z PHP na PHPsock server, ktery umoznuje komunikaci v realnem case s klienty.
+ *
+ * @author     jd
+ * @version    1.0.0
+ * @since      2014-06-20
+ *
+ * @package    Ebrana\PHPsock
+ * @copyright  Ebrana s.r.o. (http://ebrana.cz)
+ * @license    Ebrana Licence
+ */
 class Connector {
 
-	const DEFAULT_PORT = 7070;
-
-	protected $_dnode;
+    protected $_port;
+    protected $_host;
+    protected $_dnode;
 	protected $_isConnected = false;
 	protected $_connection;
 
-	public function __construct() {
+    const DEFAULT_PORT = 7070;
+    const DEFAULT_HOST = 'afrodita.ebrana.cz';
+
+    /**
+     * Místo konstruktoru je lepsi pouzit tovarnu create, ktera umznuje prepsani parametru z konfigurace
+     *
+     * @param null|int $port
+     * @param null|string $host
+     * @return Connector
+     */
+    public function __construct($port = null, $host = null) {
+        $this->_port = empty($port) ? self::DEFAULT_PORT : $port;
+        $this->_host = empty($host) ? self::DEFAULT_HOST : $host;
+
 		$this->_dnode = new \DnodeSyncClient\Dnode();
+
+        if ($this->_connection = $this->_dnode->connect($this->_host, $this->_port)) {
+            $this->_isConnected = true;
+        }
 	}
 
-	public function connect($port = 7070, $host = 'localhost') {
-		$this->_connection = $this->_dnode->connect($host, $port);
-		$this->_isConnected = true;
-	}
+    /**
+     * Továrna pro vytvoření konektoru pro připojení k NodeJS realtime serveru
+     *
+     * @author Jan Dvořák
+     * @param null|array $config
+     *
+     * @return Connector
+     * @throws \Ebrana\Exception
+     */
+    public static function create($config = null)
+    {
+        $defaultConfig = array(
+            'port' => self::DEFAULT_PORT,
+            'host' => self::DEFAULT_HOST
+        );
+
+        if(is_null($config))
+        {
+            $app = \Zend_Registry::get('App')->getOption('app');
+            if (isset($config['phpsock'])) {
+                $config = $app['phpsock'];
+                if (empty($config['port'])) $config['port'] = self::DEFAULT_PORT;
+                if (empty($config['host'])) $config['host'] = self::DEFAULT_HOST;
+            } else {
+                $config = array();
+            }
+        }
+
+        $config = array_merge($defaultConfig, $config);
+
+        if(null === $config['port'] || null === $config['host'])
+        {
+            throw new \Ebrana\Exception(5001, 'Some required (port, host) parameter missing in config.');
+        }
+
+        return new self(
+            $config['port'],
+            $config['host']
+        );
+    }
 
 	public function getOnlineClients() {
 		$this->_checkConnection();
@@ -36,8 +98,9 @@ class Connector {
 	}
 
 	protected function _checkConnection() {
+        // TODO try to reconnect
 		if (!$this->_isConnected) {
-			throw new Exception('Not connected.');
+			throw new Exception('Not connected to PHPsock server.');
 		}
 	}
 
